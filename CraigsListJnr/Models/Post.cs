f7d6e7f7c4dd7c43e59d50d4e68c12b274dc1622
sleepysaurus <ylegaspi@gmail.com>
2015-05-05 15:51:02 +1200

    Init

---
+++ CraigsListJnr/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CraigsListJnr.Models
+{
+    public class Post
+    {
+        public int Id { get; set; } 
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public string Email { get; set; }
+        public string Key { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+    }
+}
